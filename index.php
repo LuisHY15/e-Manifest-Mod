95de02743513819c4c3b15da6e8f257471e31363
luis <luis.hyrb.15@hotmail.com>
2016-04-12 17:57:20 -0500

    e-Manifest M1

---
+++ index.php
@@ -0,0 +1,365 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <!-- Meta Tags -->
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="description" content="">
+   <meta name="author" content="">
+    
+   <title>e-Manifest - Multipurpse Landing Page</title>
+
+   <link rel="shortcut icon" href="images/favicon.png">
+   
+   <!-- Bootstrap Core CSS -->
+   <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+   <!-- Custom CSS -->
+   <link rel="stylesheet" href="css/style.css" type="text/css">
+   <!-- Animate CSS -->
+   <link rel="stylesheet" href="css/animate.css">
+   <!-- Light Box CSS -->
+   <link href="css/lightbox.css" rel="stylesheet" />
+   <!-- Custom Fonts -->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300,800' rel='stylesheet' type='text/css'>
+
+   <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+  
+   <!-- Important Owl stylesheet -->
+   <link rel="stylesheet" href="css/owl.carousel.css">
+   <!-- Default Theme Owl Carousel -->
+   <link rel="stylesheet" href="css/owl.theme.css">
+   <!-- jQuery 1.9.1 -->
+   <script src="js/jquery-1.9.1.min.js"></script>
+   <!-- Include js plugin -->
+   <script src="js/owl.carousel.js"></script>
+   <!-- Light Box -->
+   <script src="js/lightbox.min.js"></script>
+</head>
+
+
+<body>
+	<!-- Header -->
+	<header id="topheader">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<h1 class="text-uppercase">e-Manifest</h1>
+				</div>
+				
+				<div class="col-md-7 col-md-offset-2">
+           			 <nav class="navigation"> <!-- Navigation -->
+           			 	<ul id="menu">
+            					<li><a href="#feautures">Feautures</a></li>
+            					<li><a href="#services">Services</a></li>
+            					<li><a href="#testimonials">Testimonials</a></li>
+            					<li><a href="#portfolio">Portfolio</a></li>
+            					<li><a href="#team">Our Team</a></li>
+            					<li><a href="#pricing">Pricing</a></li>
+            					<li><a href="#contact">Contact</a></li>
+            			</ul>
+        			 </nav> <!-- Navigation - End -->
+        			 <a href="#" id="toggle"><i class="fa fa-bars text-left"></i></a>
+				</div>
+			</div>
+			
+			<div class="row margin-t-90">
+				<div class="col-md-12">
+					<h1 class="text-center wow bounceInLeft">Start filing e-Manifest today.</h1>
+					<h4 class="text-center wow bounceInRight">Save time & money with our web portal and premium support.</h4>
+					<h4 class="text-center wow bounceInRight">We provide full e-Filing Services.</h4>
+					<div class="row margin-t-60">
+						<div class="col-md-2 col-md-offset-4">
+							<a href="#subscribe" class="btn btn-primary w100 text-uppercase wow bounceInLeft">Get Started</a>
+						</div>
+						
+						<div class="col-md-2">
+							<a href="#contact" class="btn btn-success w100 text-uppercase wow bounceInRight">Contact</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<a href="#about-timeline" class="navscroll"><div class="arrow-down animated infinite bounce">
+			</div></a>
+		</div>
+	</header>
+	<!-- Header End -->
+	
+	<!-- About -->
+	<div id="about-timeline">
+		<div class="container">
+			<h1 class="text-center text-uppercase">IMEXSOL e-Manifest Features:</h1>
+			
+			<div class="row">
+				<div class="col-lg-12">
+				<ul>
+					<li>
+						<div class="timeline-left">
+							<div class="timeline-image wow zoomIn">
+								<img src="images/image1-s.jpg" alt="">
+							</div>
+							<br class="clearfix">
+							<h3 class="wow bounceInLeft">Empty trips by batch</h3>
+							<p class="wow bounceInLeft">Select  a profile and how many trips it’s all you need.</p>
+						</div>
+					</li>
+						
+					<li>	
+						<div class="timeline-right">
+							<div class="timeline-image wow zoomIn">
+								<img src="images/image2-s.jpg" alt="">
+							</div>
+							<h3 class="wow bounceInRight">Automatic email trip released</h3>
+							<p class="wow bounceInRight">hold and arrival notifications.</p>
+						</div>
+					</li>
+					
+					<li>
+						<div class="timeline-left">
+							<div class="timeline-image wow zoomIn">
+								<img src="images/image3-s.jpg" alt="">
+							</div>
+							<h3 class="wow bounceInLeft">Web responsive design</h3>
+							<p class="wow bounceInLeft">Access portal from your phone, tablet or computer user interface will adjust to your device.</p>
+						</div>
+					</li>
+					
+					<li>	
+						<div class="timeline-right">
+							<div class="timeline-image wow zoomIn">
+								<img src="images/image4-s.jpg" alt="">
+							</div>
+							<h3 class="wow bounceInRight">Data integrations</h3>
+							<p class="wow bounceInRight">want to use broker data to get shipment information? We can help!</p>
+						</div>
+					</li>
+					
+					<li>	
+						<div class="timeline-left">
+							<div class="timeline-image">
+								<p class="timeline-end wow bounceInUp">FEATURE END</p>
+							</div>
+						</div>
+					</li>
+				</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- About End -->
+	
+	<!-- Informations -->
+	<div id="informations">
+		<div class="container">
+			<div class="row text-center wow zoomIn">
+				<div class="col-md-12">
+					<h1 class="text-center wow bounceInLeft">Empieze a enviar e-Manifest hoy.</h1>
+					<h4 class="text-center wow bounceInRight">Ahorre tiempo y dinero con nuestro portal web y soporte premium..</h4>
+					<h4 class="text-center wow bounceInRight">Proporcionamos servicio completo de llenado de informacion..</h4>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Informations End -->
+	
+	<!-- Feautures -->
+	<div id="feautures">
+		<div class="container wow flipInX">
+			<h1 class="text-uppercase text-center">IMEXSOL e-Manifest le ofrece</h1>
+			<div class="row margin-t-30">
+				<div class="col-md-4">
+					<img src="images/monitor.png" alt="Monitor">
+				</div>
+				
+				<div class="col-md-8">
+				<div class="row">
+					<div class="col-md-6">
+						<h3><i class="fa fa-rocket fa-lg"></i> Generacion de manifiestos vacios en lote</h3>
+						<p>seleccione perfil y cantidad de manifiestos, es todo lo que necesita.</p>
+					</div>
+				
+					<div class="col-md-6">
+						<h3><i class="fa fa-rocket fa-lg"></i> Notificaciones automaticas via email</h3>
+						<p>notifique cruce, rojo operative y presentacion en el puerto.</p>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-md-6">
+						<h3><i class="fa fa-rocket fa-lg"></i> Interfaz web inteligente</h3>
+						<p>Se ajustara a su pantalla de cellular, tableta o ordenador.</p>
+					</div>
+				
+					<div class="col-md-6">
+						<h3><i class="fa fa-rocket fa-lg"></i> Integracion de datos</h3>
+						<p>Quiere integrar la inforacion de su broker y emanifest, podemos ayudarle.</p>
+					</div>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Feautures End -->
+	
+			<!-- Testimonials -->
+	<div id="testimonials">
+		<div class="container">
+			<h1 class="text-uppercase text-center">Our team</h1>
+			
+			<div class="row margin-t-30">
+				<div class="col-md-5">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="testimonial-circle">
+								<img src="images/profile2.jpg" alt="User">
+							</div>
+						</div>
+						
+						<div class="col-md-9">
+						<div class="testimonial-group">
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+						</div>
+							<h4>Lorem Ipsulm, <span class="text-small">c# developer</span></h4>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-md-5 col-md-offset-1">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="testimonial-circle">
+								<img src="images/profile1.jpg" alt="User">
+							</div>
+						</div>
+						
+						<div class="col-md-9">
+						<div class="testimonial-group">
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+						</div>
+							<h4>Lorem Ipsulm, <span class="text-small">web developer</span></h4>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row margin-t-60">
+				<div class="col-md-5">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="testimonial-circle">
+								<img src="images/profile3.jpg" alt="User">
+							</div>
+						</div>
+						
+						<div class="col-md-9">
+						<div class="testimonial-group">
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+						</div>
+							<h4>Lorem Ipsulm, <span class="text-small">web designer</span></h4>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-md-5 col-md-offset-1">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="testimonial-circle">
+								<img src="images/profile4.jpg" alt="User">
+							</div>
+						</div>
+						
+						<div class="col-md-9">
+						<div class="testimonial-group">
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+						</div>
+							<h4>Lorem Ipsulm, <span class="text-small">java designer</span></h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Testimonials End -->
+
+	
+	<!-- Contact -->
+	<div id="contact">
+		<div class="container">
+			<h1 class="text-center text-uppercase">Contact US</h1>
+			<div class="row margin-t-30">
+			
+			<div class="col-md-6">
+			<div class="contactbg">
+			<form action="#" method="post">
+			<div class="row">
+				<h3 class="text-center text-uppercase">Stay tuned</h3>
+				<div class="col-md-6">
+					<input type="text" id="name" name="name" placeholder="Name" required>
+				</div>
+				
+				<div class="col-md-6">
+					<input type="email" id="email" name="email" placeholder="E-mail" required>
+				</div>
+			</div>
+				<textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+				<input type="submit" id="contactbtn" name="contactbtn" value="SEND MESSAGE">
+			</form>
+			</div>
+			</div>
+			
+			<div class="col-md-6">
+				<h4 class="text-uppercase">Keep In Touch</h4>
+				<p>Lorem ipsum dolor sit amet, consectetured adipiscing elit, sed do eiusmod tempor inci didunt ut labore et dolore magna.</p>
+				<div class="row">
+					<div class="col-md-1 col-md-offset-1 margin-t-30">
+						<i class="fa fa-building-o"></i>
+					</div>
+					<div class="col-md-10 margin-t-30">
+						<h3>Our Office</h3>
+						<p></p>
+					</div>
+					
+					<div class="col-md-1 col-md-offset-1 margin-t-30">
+						<i class="fa fa-phone"></i>
+					</div>
+					<div class="col-md-10 margin-t-30">
+						<h3>Our Phone</h3>
+						<p>1-956-242-0688 </p>
+					</div>
+					
+					<div class="col-md-1 col-md-offset-1 margin-t-30">
+						<i class="fa fa-envelope-o"></i>
+					</div>
+					<div class="col-md-10 margin-t-30">
+						<h3>Our Mail</h3>
+						<p>sales@imexsol.com</p>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
+	<!-- Contact End -->
+	
+	<!-- Footer -->
+	<footer>
+		<div class="container text-center">
+			<a href="#" id="back-to-top"><i class="fa fa-arrow-up"></i></a>
+			<p class="text-center">2015 © Copyright Canis. All rights Reserved.</p>
+		</div>
+	</footer>
+	<!-- Footer End -->
+	
+    
+	<!-- Bootstrap -->
+    <script src="js/bootstrap.js"></script> 
+    <!-- WOW -->
+    <script src="js/wow.min.js"></script>
+
+	<!-- Canis Custom JS -->
+	<script src="js/canis.js" type="text/javascript"></script>
+	
+</body>
+</html>
